6a02fcf7c13cf956feed5854038d8d2f4d105626
Rachael Lunney <rlunney@gmail.com>
2017-07-24 20:57:02 -0500

    Add ability to POST name

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/greeting/')
+@app.route('/greeting/', methods=['GET', 'POST'])
 def greeting():
-  return 'Hello, Guest'
+    name = 'Guest'
+    if request.method == 'POST':
+        name = post_name(request.form['name'])
+    return 'Hello, {0} \n'.format(name)
+
+def post_name(name):
+    file = open('name.txt', 'w')
+    file.write(name)
+    return name
